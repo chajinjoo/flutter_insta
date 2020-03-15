e967efd6497c8d542a9fc83da3c7a4607017221c
chajinjoo <chajanee@gmail.com>
2020-03-15 21:03:19 +0900

    프로필화면 커스텀 appBar, slide menu rayout 틀 잡음

--- lib/screens/profile_page.dart
+++ lib/screens/profile_page.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutterinsta/constants/size.dart';
 
 //Provider를 사용하니까 stl 위젯을 써도 되지만,
 //ProfilePage 에서는 애니메이션을 사용하기 때문에 stful 위젯을 사용해준다!
@@ -26,14 +27,6 @@ class _ProfilePageState extends State<ProfilePage> {
           _profile(),
         ],
       ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: () {
-          setState(() {
-            //플로팅액션버튼 클릭하면 _menuOpened를 true 면 false 로~ false 면 true 로 바꿈
-            _menuOpened = !_menuOpened;
-          });
-        },
-      ),
     );
   }
 
@@ -42,7 +35,7 @@ class _ProfilePageState extends State<ProfilePage> {
     return AnimatedContainer(
       //커브 효과
       curve: Curves.easeInOut,
-      color: Colors.redAccent,
+      color: Colors.grey[200],
       duration: Duration(milliseconds: duration),
       //세로 y축과 앞뒤 z축은 변경 안해주니 0으로 세팅
       transform: Matrix4.translationValues(
@@ -51,13 +44,28 @@ class _ProfilePageState extends State<ProfilePage> {
           _menuOpened ? _size.width - menuWidth : _size.width,
           0,
           0),
+      child: SafeArea(
+        //메뉴를 클릭했을때 해당 column 사이즈가 활성화된 화면 전체를 차지하게끔
+        child: SizedBox(
+          width: menuWidth,
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: <Widget>[
+              FlatButton(
+                onPressed: () {},
+                child: Text('chacha__dev'),
+              ),
+            ],
+          ),
+        ),
+      ),
     );
   }
 
   Widget _profile() {
     return AnimatedContainer(
       curve: Curves.easeInOut,
-      color: Colors.yellowAccent,
+      color: Colors.transparent,
       duration: Duration(milliseconds: duration),
       //세로 y축과 앞뒤 z축은 변경 안해주니 0으로 세팅
       transform: Matrix4.translationValues(
@@ -65,6 +73,41 @@ class _ProfilePageState extends State<ProfilePage> {
           _menuOpened ? -menuWidth : 0,
           0,
           0),
+      //화면 바깥에 탈모부분 안건드리게 safearea로 감싸줌
+      child: SafeArea(
+        child: Column(
+          children: <Widget>[
+            _usernameIconButton(),
+          ],
+        ),
+      ),
+    );
+  }
+
+  Row _usernameIconButton() {
+    return Row(
+      children: <Widget>[
+        Expanded(
+            child: Padding(
+          padding: const EdgeInsets.only(left: common_gap),
+          child: Text(
+            'chacha__dev',
+            style: TextStyle(
+              fontWeight: FontWeight.bold,
+              fontSize: 20.0,
+            ),
+          ),
+        )),
+        IconButton(
+          icon: Icon(Icons.menu),
+          onPressed: () {
+            setState(() {
+              //플로팅액션버튼 클릭하면 _menuOpened를 true 면 false 로~ false 면 true 로 바꿈
+              _menuOpened = !_menuOpened;
+            });
+          },
+        ),
+      ],
     );
   }
 }
